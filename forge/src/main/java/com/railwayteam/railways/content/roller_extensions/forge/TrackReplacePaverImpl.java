e4769d720cd8140b92114b2446b74953f18e45fc
IThundxr <contact@ithundxr.dev>
2023-11-02 16:22:45 -0400

    patch f compat

--- forge/src/main/java/com/railwayteam/railways/content/roller_extensions/forge/TrackReplacePaverImpl.java
+++ forge/src/main/java/com/railwayteam/railways/content/roller_extensions/forge/TrackReplacePaverImpl.java
@@ -1,13 +1,13 @@
 package com.railwayteam.railways.content.roller_extensions.forge;
 
 import com.simibubi.create.content.contraptions.behaviour.MovementContext;
-import com.simibubi.create.content.logistics.filter.FilterItem;
 import com.simibubi.create.foundation.item.ItemHelper;
 import net.minecraft.world.item.ItemStack;
 
 public class TrackReplacePaverImpl {
+    //fixme properly fix this once i port create fabric
     public static ItemStack extract(ItemStack filter, MovementContext context, int amt) {
         return ItemHelper.extract(context.contraption.getSharedInventory(),
-                stack -> FilterItem.test(context.world, stack, filter), amt, false);
+                stack -> context.getFilterFromBE().test(context.world, stack), amt, false);
     }
 }
